cb20b6cf264c4f8541c2f3f82ebcdbf746b86928
Filippo0420 <fpawelec0@gmail.com>
2022-11-15 15:19:18 +0100

    Wstępna implementacja pobierania lokalizacji urządzenia

--- app/src/main/java/com/fpp/ct/MainActivity.kt
+++ app/src/main/java/com/fpp/ct/MainActivity.kt
@@ -1,32 +1,58 @@
 package com.fpp.ct
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.fpp.ct.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+
+//import com.google.android.gms.location.LocationServices;
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
     private lateinit var imageView: ImageView
-    val REQUEST_IMAGE_CAPTURE = 111
     private lateinit var binding: ActivityMainBinding
+    private lateinit var tekst: TextView
+
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var txt1:TextView
+    private lateinit var txt2:TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
         button = findViewById(R.id.baton)
         imageView = findViewById(R.id.imydz)
+        txt1 = findViewById(R.id.tekstsicior1)
+        txt2 = findViewById(R.id.tekstsicior2)
+
+        getCurrentLocation()
 
         binding.baton.isEnabled = false
+
 
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED)
         {
@@ -40,18 +66,19 @@ class MainActivity : AppCompatActivity() {
         binding.baton.setOnClickListener {
             var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(i,105)
-            Toast.makeText(this,"pressed",Toast.LENGTH_SHORT)
+
         }
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode==105) {
+        if (requestCode==105) {
             val binding = ActivityMainBinding.inflate(layoutInflater)
             setContentView(binding.root)
             var mojobraz: Bitmap?
             mojobraz = data?.getParcelableExtra<Bitmap>("data")
-            //binding.imydz.setImageBitmap(mojobraz)
+            binding.imydz.setImageBitmap(mojobraz)
             Toast.makeText(this,"ok", Toast.LENGTH_SHORT)
             findViewById<ImageView>(R.id.imydz).setImageBitmap(mojobraz)
         }
@@ -64,5 +91,87 @@ class MainActivity : AppCompatActivity() {
             setContentView(binding.root)
             binding.baton.isEnabled=true
         }
+        if(requestCode == PERMISSION_REQUEST_ACCESS_LOCATION){
+            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            {
+                Toast.makeText(this, "Granted", Toast.LENGTH_SHORT).show()
+                getCurrentLocation()
+            }
+            else
+            {
+                Toast.makeText(this, "Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
+    private fun getCurrentLocation(){
+        if(checkPermissions())
+        {
+            if(isLocationEnabled())
+            {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    requestPermission()
+                    return
+                }
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener(this){ task->
+                    val location: Location?=task.result
+                    if(location==null)
+                    {
+                        Toast.makeText(this,"Null recived",Toast.LENGTH_SHORT).show()
+                    }
+                    else
+                    {
+                        Toast.makeText(this,"Success",Toast.LENGTH_SHORT).show()
+                        txt1.text = "1:"+location.latitude
+                        txt2.text = "2:"+location.longitude
+                    }
+                }
+            }
+            else
+            {
+                Toast.makeText(this,"Turn on location",Toast.LENGTH_SHORT).show()
+                val intent=Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+        else
+        {
+            requestPermission()
+        }
+    }
+
+    private fun isLocationEnabled():Boolean{
+        val locationManager:LocationManager=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)||locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+    companion object {
+        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+    }
+    private fun checkPermissions():Boolean{
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+            ==PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            ==PackageManager.PERMISSION_GRANTED)
+        {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermission(){
+        ActivityCompat.requestPermissions(
+            this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_ACCESS_LOCATION
+        )
+    }
+
 }
